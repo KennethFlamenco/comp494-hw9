8c15beb3876b03677455f7af3942609ea9ccdd15
KennethFlamenco <flamencokenneth@gmail.com>
2025-11-02 14:50:32 -0800

    done first 3 execrises

--- hw9-kf.js
+++ hw9-kf.js
@@ -1,1 +1,16 @@
+//Excerise 1
+camelCase = 3 + 5 * (10 / 2) - (8 - 4);
+console.log(camelCase);
 
+//Excerise 2
+var projectIdentifier;
+projectIdentifier = "COMP484";
+//this fails because JS is case senstive and will assign it to a new variable while the original variable is still COMP484
+ProjectIdentifier = "JS_Advanced";
+projectIdentifier = "JS_Advanced";
+console.log(projectIdentifier);
+
+//Excerise 3
+var courseDescription;
+courseDescription = "The course is \"interactive\" and involves 'scripting' logic.";
+console.log(courseDescription);
